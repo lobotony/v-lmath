42e9549dbe86f74248ff0612efa981cc97239aee
Tony Kostanjsek <708330+lobotony@users.noreply.github.com>
2019-09-17 21:35:29 +0200

    update

--- lmath.v
+++ lmath.v
@@ -18,7 +18,7 @@ pub fn clamp01(val f32) f32 {
     return clamp(val, 0.0, 1.0)
 }
 
-// compate two float numbers, taking 32bit inaccuracies into account by allowing for deviations up to FLT_EPSILON
+// compare two float numbers, taking 32bit inaccuracies into account by allowing for deviations up to FLT_EPSILON
 pub fn equalsf(v0 f32, v1 f32) bool {
     return C.fabsf(v0 - v1) < C.FLT_EPSILON
 }
